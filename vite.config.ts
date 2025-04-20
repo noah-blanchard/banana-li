ba75f207cfe049a6177e55113bfa1ef81bedb77d
Noah Blanchard <n.blanchard190302@gmail.com>
2025-04-20 00:23:51 -0400

    edit base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/banana-li/',
   plugins: [react(), VitePWA({
     strategies: 'injectManifest',
     srcDir: 'src',
